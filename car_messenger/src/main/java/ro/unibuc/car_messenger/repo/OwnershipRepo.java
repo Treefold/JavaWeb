d9a92ecf719a4daf2ec514cd604a75d4ce017209
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-01-09 19:59:53 +0200

    Add repo addnotation

--- car_messenger/src/main/java/ro/unibuc/car_messenger/repo/OwnershipRepo.java
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/repo/OwnershipRepo.java
@@ -1,7 +1,7 @@
 package ro.unibuc.car_messenger.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ro.unibuc.car_messenger.domain.Car;
 import ro.unibuc.car_messenger.domain.Ownership;
 import ro.unibuc.car_messenger.domain.User;
@@ -9,6 +9,7 @@ import ro.unibuc.car_messenger.domain.User;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OwnershipRepo extends JpaRepository<Ownership, Long> {
 
     List<Ownership> findAllByUserId (Long userId);
--- car_messenger/src/main/java/ro/unibuc/car_messenger/repo/UserRepo.java
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/repo/UserRepo.java
@@ -1,10 +1,12 @@
 package ro.unibuc.car_messenger.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ro.unibuc.car_messenger.domain.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
     Optional<User> findByUsername(String username);
