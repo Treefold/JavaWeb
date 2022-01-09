14ab662cad40fa3a9e2e8b129d6613bfb2e02548
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-01-09 20:35:10 +0200

    cleaning

--- car_messenger/src/main/java/ro/unibuc/car_messenger/service/OwnershipService.java
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/service/OwnershipService.java
@@ -6,13 +6,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ro.unibuc.car_messenger.domain.Ownership;
 import ro.unibuc.car_messenger.domain.OwnershipType;
-import ro.unibuc.car_messenger.dto.CarDto;
 import ro.unibuc.car_messenger.dto.OwnershipDto;
-import ro.unibuc.car_messenger.dto.UserDto;
 import ro.unibuc.car_messenger.exception.UniqueException;
-import ro.unibuc.car_messenger.mapper.CarMapper;
 import ro.unibuc.car_messenger.mapper.OwnershipMapper;
-import ro.unibuc.car_messenger.mapper.UserMapper;
 import ro.unibuc.car_messenger.repo.OwnershipRepo;
 
 import java.util.List;
@@ -24,17 +20,6 @@ public class OwnershipService {
     private OwnershipRepo ownershipRepo;
     @Autowired
     private OwnershipMapper ownershipMapper;
-    @Autowired
-    private UserMapper userMapper;
-    @Autowired
-    private CarMapper carMapper;
-
-    public OwnershipService(OwnershipRepo ownershipRepo, OwnershipMapper ownershipMapper, UserMapper userMapper, CarMapper carMapper) {
-        this.ownershipRepo = ownershipRepo;
-        this.ownershipMapper = ownershipMapper;
-        this.userMapper = userMapper;
-        this.carMapper = carMapper;
-    }
 
     public OwnershipDto saveOwnership (OwnershipDto ownershipDto) {
         Ownership ownership = ownershipMapper.mapToEntity(ownershipDto);
@@ -45,13 +30,9 @@ public class OwnershipService {
         return ownershipMapper.mapToDto(ownershipRepo.save(ownership));
     }
 
-    public List<OwnershipDto> findAll() { return ownershipMapper.mapToDto(ownershipRepo.findAll()); }
     public List<OwnershipDto> findAllByUserId (Long userId) { return ownershipMapper.mapToDto(ownershipRepo.findAllByUserId(userId)); }
     public List<OwnershipDto> findAllByCarId  (Long carId)  { return ownershipMapper.mapToDto(ownershipRepo.findAllByCarId (carId)); }
     public Optional<OwnershipDto> findFirstByUserIdAndCarId (Long userId, Long carId) { return ownershipMapper.mapToDto(ownershipRepo.findFirstByUserIdAndCarId(userId, carId)); }
-    public Optional<OwnershipDto> findFirstByUserAndCar (UserDto userDto, CarDto carDto) {
-        return ownershipMapper.mapToDto(ownershipRepo.findFirstByUserAndCar(userMapper.mapToEntity(userDto), carMapper.mapToEntity(carDto)));
-    }
 
     public Optional<OwnershipDto> updateOwnership (Long id, OwnershipType category) {
         Optional<Ownership> ownership = ownershipRepo.findById(id);
