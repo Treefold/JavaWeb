641667e2b775592bfe101d8773373a4d0e7066a5
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-01-10 02:11:10 +0200

    UserController - updatePassword tests

--- car_messenger/src/test/java/ro/unibuc/car_messenger/controller/UserControllerTest.java
+++ car_messenger/src/test/java/ro/unibuc/car_messenger/controller/UserControllerTest.java
@@ -2,24 +2,18 @@ package ro.unibuc.car_messenger.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import ro.unibuc.car_messenger.domain.User;
 import ro.unibuc.car_messenger.dto.UserDto;
 import ro.unibuc.car_messenger.exception.InvalidNewUserException;
+import ro.unibuc.car_messenger.exception.UserNotLoggedinException;
 import ro.unibuc.car_messenger.service.CarService;
 import ro.unibuc.car_messenger.service.OwnershipService;
 import ro.unibuc.car_messenger.service.UserService;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.never;
@@ -66,15 +60,47 @@ class UserControllerTest {
         when(userService.saveUser(any())).thenReturn(userDtoIn);
 
         //Act
-        MvcResult result = mockMvc.perform(post("/user/create")
+        mockMvc.perform(post("/user/create")
                         .content(objectMapper.writeValueAsString(userDtoIn))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andReturn();
+                .andExpect(status().isCreated());
 
         //Assert
         verify(userService, times(1)).saveUser(any());
         verify(userService, times(1)).addRoleToUser(any(), any());
-        assertThat(result.getResponse().getContentAsString()).isEqualTo(objectMapper.writeValueAsString(userDtoIn));
     }
+
+    @Test
+    void updateUserPassword_UserNotFoundErr() throws Exception {
+        //Arrange
+        String newPassword = "new-password";
+        when(userService.handleLogin(any(), any())).thenThrow(new UserNotLoggedinException());
+
+        //Act
+        mockMvc.perform(put("/user/updatePassword")
+                        .content(newPassword)
+                        .contentType(MediaType.TEXT_PLAIN))
+                .andExpect(status().isForbidden());
+
+        //Assert
+        verify(userService, times(1)).handleLogin(any(), any());
+        verify(userService, never()).updateUser(any(), any());
+    }
+
+    @Test
+    void updateUserPassword_NoError() throws Exception {
+        //Arrange
+        String newPassword = "new-password";
+
+        //Act
+        mockMvc.perform(put("/user/updatePassword")
+                        .content(newPassword)
+                        .contentType(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk());
+
+        //Assert
+        verify(userService, times(1)).handleLogin(any(), any());
+        verify(userService, times(1)).updateUser(any(), any());
+    }
+
 }
