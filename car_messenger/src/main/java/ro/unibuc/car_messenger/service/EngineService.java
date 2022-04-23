099b9933735e4c4209ca0690ad6d8e3756cfbb4e
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-04-23 15:26:35 +0300

    Add Engine Service

---
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/service/EngineService.java
@@ -0,0 +1,34 @@
+package ro.unibuc.car_messenger.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ro.unibuc.car_messenger.dto.EngineDto;
+import ro.unibuc.car_messenger.exception.UniqueException;
+import ro.unibuc.car_messenger.mapper.EngineMapper;
+import ro.unibuc.car_messenger.repo.EngineRepo;
+
+@Service @Transactional @Slf4j
+public class EngineService {
+    @Autowired
+    private EngineRepo engineRepo;
+    @Autowired
+    private EngineMapper engineMapper;
+
+    public EngineDto saveEngine (EngineDto engineDto)
+    {
+        if (engineRepo.findFirstByNumber(engineDto.getNumber()).isPresent()) {
+            throw  new UniqueException("The engine already exists");
+        }
+        log.info("Saving new {} engine {} to the database", engineDto.getType(), engineDto.getNumber());
+        return engineMapper.mapToDto(engineRepo.save(engineMapper.mapToEntity(engineDto)));
+    }
+
+    public void deleteEngine(Long id) {
+        log.info("Deleting engine Id{{}} from the database", id);
+        engineRepo.deleteById(id);
+    }
+
+}
+
