61687b007fb70bbe5cc3eb8128e1ca16848ee226
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-04-24 12:00:46 +0300

    add security

---
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package ro.unibuc.car_messenger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("mihaidaniel@gmail.com")
+                .password(passwordEncoder().encode("Password0."))
+                .roles("ADMIN")
+                .and()
+                .withUser("Test1@gmail.com")
+                .password(passwordEncoder().encode("Test1."))
+                .roles("User");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/login.do*").permitAll()
+                .and()
+                .formLogin().loginPage("/loginForm")
+                .loginProcessingUrl("/authUser")
+                .failureUrl("/login-error").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/access_denied");
+    }
+}
