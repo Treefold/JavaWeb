59cefabda0d4f6fe3621e3a338920ad1b52b6593
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-04-25 18:03:58 +0300

    Update Homepage

--- car_messenger/src/main/java/ro/unibuc/car_messenger/controller/HomeController.java
+++ car_messenger/src/main/java/ro/unibuc/car_messenger/controller/HomeController.java
@@ -1,5 +1,6 @@
 package ro.unibuc.car_messenger.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,9 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ro.unibuc.car_messenger.dto.CarDto;
+import ro.unibuc.car_messenger.dto.OwnershipDto;
+import ro.unibuc.car_messenger.service.CarService;
+import ro.unibuc.car_messenger.service.OwnershipService;
+import ro.unibuc.car_messenger.service.UserService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private CarService carService;
+    @Autowired
+    private OwnershipService ownershipService;
 
     @GetMapping({"login","/loginForm"})
     public String loginForm() {
@@ -36,6 +50,25 @@ public class HomeController {
     @RequestMapping("/")
     public ModelAndView getHome() {
         ModelAndView model = new ModelAndView("home");
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long userId = userService.getUser(username).get().getId();
+
+        List<OwnershipDto> ownerships = ownershipService.findAllByUserId(userId);
+        List<CarDto> ownedCars = ownerships.stream().filter(o -> o.isOwner())
+                .map(ownershipDto -> ownershipDto.getCarDto()).toList();
+        List<CarDto> coownedCars = ownerships.stream().filter(o -> o.isCoowner())
+                .map(ownershipDto -> ownershipDto.getCarDto()).toList();
+        List<CarDto> invitations = ownerships.stream().filter(o -> o.isInvited())
+                .map(ownershipDto -> ownershipDto.getCarDto()).toList();
+        List<CarDto> requests = ownerships.stream().filter(o -> o.isRequested())
+                .map(ownershipDto -> ownershipDto.getCarDto()).toList();
+
+        model.addObject("ownedCars", ownedCars);
+        model.addObject("coownedCars", coownedCars);
+        model.addObject("invitations", invitations);
+        model.addObject("requests", requests);
+
         return model;
     }
 
