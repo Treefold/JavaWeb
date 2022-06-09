f825a6143842395aa70d92fa5b584eaae34a61aa
Mihai Daniel Surcea <mihai-daniel.surcea@my.fmi.unibuc.ro>
2022-06-09 08:50:11 +0300

    forgotten

--- car_messenger/src/test/java/ro/unibuc/car_messenger/CarMessengerApplicationTests.java
+++ car_messenger/src/test/java/ro/unibuc/car_messenger/CarMessengerApplicationTests.java
@@ -2,8 +2,10 @@ package ro.unibuc.car_messenger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class CarMessengerApplicationTests {
 
     @Test
